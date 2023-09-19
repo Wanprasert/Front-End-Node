b234eb7e7a714dbb048ee6d7c1bed48795ea9d23
Your Name <you@example.com>
2023-09-19 11:25:47 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -6,21 +6,17 @@ const axios = require('axios');
 const app = express(); 
 var bodyParser = require('body-parser'); 
 
-
 // Base URL for the API 
 //const base_url = "https://api.example.com"; 
 const base_url = "http://localhost:3000"; 
-
 
 // Set the template engine 
 app.set('view engine', 'ejs'); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 // Serve static files 
 app.use(express.static (__dirname + '/public')); 
-
 
 app.get("/", async (req, res) => { 
     try { 
